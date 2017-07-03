b1d3f0ad7d8b56b7400ee4843af42f01c41ecd87
Frizi <frizi09@gmail.com>
2017-07-03 23:18:18 +0200

    Early attempt to connect to launchpad

--- src/App.vue
+++ src/App.vue
@@ -1,68 +1,163 @@
 <template lang="html">
-    <pre>{{ notesDesc }}</pre>
+    <div>
+        <!-- <select v-model="oscType">
+            <option value="sine">sine</option>
+            <option value="square">square</option>
+            <option value="sawtooth">sawtooth</option>
+            <option value="triangle">triangle</option>
+        </select> -->
+        <pre>{{ notesDesc }}</pre>
+    </div>
 </template>
 
 <script>
 import {noteFrequency, noteName, keyToNote} from './notes'
 import {isDef, isUndef} from './util'
+import R from 'ramda'
+import SynthNote from './SynthNote'
+import webmidi from 'webmidi'
+
+window.webmidi = webmidi
 
 export default {
     data () {
         return {
             octave: 5,
-            osc: {}
+            oscType: 'square',
+            keyboardState: {},
+            extMidiState: {}
         }
     },
     mounted () {
         this._audioCtx = new AudioContext({latencyHint: 'interactive'})
         this._master = this._audioCtx.createGain()
-        this._master.connect(this._audioCtx.destination)
-        this.osc = {}
+        this._outCompress = this._audioCtx.createDynamicsCompressor()
+
+        this._master.connect(this._outCompress)
+        this._outCompress.connect(this._audioCtx.destination)
+        this._synths = {}
+        this.midiInput = null
+        this.midiOutput = null
+        this.setupMidi()
+    },
+    methods: {
+        setupMidi() {
+            webmidi.enable(() => {
+                const instantInput = webmidi.getInputByName('Launchpad')
+                const instantOutput = webmidi.getOutputByName('Launchpad')
+                instantInput && this.connectMidiInput(instantInput)
+                instantOutput && this.connectMidiOutput(instantOutput)
+
+                webmidi.addListener('connected', (e) => {
+                    if (e.name.indexOf('Launchpad') >= 0) {
+                        if (e.input) {
+                            this.connectMidiInput(e.input)
+                        }
+                        if (e.output) {
+                            this.connectMidiOutput(e.output)
+                        }
+                    }
+                })
+            })
+        },
+        connectMidiInput (input) {
+            console.log('connectMidiInput')
+            if (this.midiInput === input) return
+            this.midiInput = input
+            this.midiInput.addListener('noteon', "all", (e) => {
+                this.$set(this.extMidiState, e.note.number, true)
+            })
+            this.midiInput.addListener('noteoff', "all", (e) => {
+                this.$set(this.extMidiState, e.note.number, false)
+            })
+        },
+        connectMidiOutput (output) {
+            if (this.midiOutput === output) return
+
+            console.log('connectMidiOutput')
+            this.midiOutput = output
+            this.midiOutput.sendControlChange(0, 0)
+            this.midiOutput.sendControlChange(0, 2)
+        },
+        createOsc (note) {
+            if (isUndef(this._synths[note])) {
+                const synth = new SynthNote(this._audioCtx, note)
+                synth.playOn(this._master)
+                this.$set(this._synths, note, synth)
+            }
+        },
+        removeOsc (note) {
+            if (isDef(this._synths[note])) {
+                this._synths[note].destroy()
+                delete this._synths[note]
+            }
+        }
     },
     docEvents: {
         keydown (event) {
             const key = event.key.toLowerCase()
             switch (key) {
             case 'z':
-                this.octave = Math.max(3, this.octave - 1)
+                this.octave = Math.max(0, this.octave - 1)
                 break
             case 'x':
-                this.octave = Math.min(12, this.octave + 1)
+                this.octave = Math.min(10, this.octave + 1)
                 break
             default:
-                const baseNote = keyToNote[event.key]
-                if (isUndef(baseNote)) break
-                const note = baseNote + this.octave * 12
-                if (isUndef(this.osc[note])) {
-                    const osc = this._audioCtx.createOscillator()
-                    osc.frequency.value = noteFrequency(note)
-                    osc.connect(this._master)
-                    osc.start()
-                    this.$set(this.osc, note, osc)
-                }
+                this.$set(this.keyboardState, key, true)
                 break
             }
-
         },
         keyup () {
             const key = event.key.toLowerCase()
-            const baseNote = keyToNote[key]
-            if (isUndef(baseNote)) return
-            for (let oct = 0; oct < 12; oct++) {
-                const note = baseNote + oct * 12
-                if (isDef(this.osc[note])) {
-                    this.osc[note].stop()
-                    this.$delete(this.osc, note)
-                }
+            switch (key) {
+                case 'z':
+                case 'x':
+                    break
+                default:
+                    this.$set(this.keyboardState, key, false)
             }
         }
     },
+    watch: {
+        playingNotes (newNotes, oldNotes) {
+            const addedNotes = R.difference(newNotes, oldNotes)
+            const removedNotes = R.difference(oldNotes, newNotes)
+            addedNotes.forEach(n => {
+                this.createOsc(n)
+                this.midiOutput && this.midiOutput.playNote(n, 1, {velocity: 1})
+            })
+            removedNotes.forEach(n => {
+                this.removeOsc(n)
+                this.midiOutput && this.midiOutput.stopNote(n, 1)
+
+            })
+        }
+    },
     computed: {
+        playingNotes () {
+            const oct = this.octave * 12
+            const keysDown = Object.keys(this.keyboardState).filter(k => !!this.keyboardState[k])
+            const keyboardNotes = keysDown
+                .map(k => keyToNote[k])
+                .filter(isDef)
+                .map(n => n + oct)
+
+            const midiNotes = Object.keys(this.extMidiState).filter(k => !!this.extMidiState[k])
+
+            return R.pipe(
+                R.uniq,
+                R.sort(diff),
+                R.filter(R.both(R.lte(0), R.gte(127)))
+            )([...keyboardNotes, ...midiNotes])
+        },
         notesDesc () {
-            return Object.keys(this.osc).map(n => `${noteName(n)}: ${noteFrequency(n)}`).join('\n')
+            return this.playingNotes.map(n => `${noteName(n)}: ${noteFrequency(n)}`).join('\n')
         }
     }
 }
+
+const diff = (a, b) => a - b
 </script>
 
 <style lang="css" scoped>
--- src/notes.js
+++ src/notes.js
@@ -1,27 +1,27 @@
-import R from 'ramda'
-
-const noteStrings = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "H", "C_NEXT"];
+const noteStrings = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
 
 export function noteFrequency ( note ) {
 	return 440 * Math.pow(2,(note-69)/12);
 }
 
-export function noteName ( note ) {
-	return noteStrings[note % 12] + (Math.floor(note / 12) - 1)
+export function noteName (note) {
+	return noteStrings[note % 12] + (Math.floor(note / 12) - 2)
 }
 
-export const keyToNote = R.map(R.indexOf(R.__, noteStrings), {
-    a: 'C',
-    w: 'C#',
-	s: 'D',
-	e: 'D#',
-	d: 'E',
-	f: 'F',
-	t: 'F#',
-	g: 'G',
-	y: 'G#',
-	h: 'A',
-	u: 'A#',
-	j: 'H',
-	k: 'C_NEXT'
-})
+export const keyToNote = {
+    a: 0, // C
+    w: 1,
+	s: 2,
+	e: 3,
+	d: 4,
+	f: 5,
+	t: 6,
+	g: 7,
+	y: 8,
+	h: 9,
+	u: 10,
+	j: 11,
+	k: 12,
+	o: 13,
+	l: 14
+}
