f749e4f559126185f34f9d7b4deff3aab5d31982
Frizi <frizi09@gmail.com>
2017-07-08 13:53:54 +0200

    Build

--- config.js
+++ config.js
@@ -1,6 +1,12 @@
 module.exports = {
     entry: 'src/index.js',
+    dist: 'docs',
     html: {
         title: 'OP-Web'
+    },
+    webpack: {
+        output: {
+            publicPath: ''
+        }
     }
 }
